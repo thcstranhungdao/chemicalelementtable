6071de215b7b28f9f5087a301fa23e254cab56b2
thcstranhungdao <145541527+thcstranhungdao@users.noreply.github.com>
2023-09-22 00:09:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var btnPage = document.getElementById("btn-page");
 
 const greenColor ="#237ACC";
-const whiteColor="#800080"
+const whiteColor="#9F52DF"
 const pinkColor = "#3A8D3A";
 
 const listElementGreen = document.getElementsByClassName("green");
